8d83dea8b1eec683911c12e41ee1d98b0cb36dac
thorben ortmann <thorben.ortmann@pwc.com>
2020-10-08 12:45:53 +0200

    added run script

---
+++ src/app/paths.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+project_root: Path = Path(__file__).absolute().parents[2]
+
+db_name: str = 'DisasterResponse.db'
+table_name: str = db_name[:-3]
+db_path: Path = project_root / 'src' / 'data' / db_name
+
+model_name: str = 'classifier_tfidf_rf.pkl'
+model_path: Path = project_root / 'src' / 'models' / model_name
