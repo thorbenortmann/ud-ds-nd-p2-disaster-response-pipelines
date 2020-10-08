97171836060c90345363d405ba1b54af87cfaa71
thorben ortmann <thorben.ortmann@pwc.com>
2020-10-08 16:08:45 +0200

    made model default name more generic and complying with README

--- src/app/paths.py
+++ src/app/paths.py
@@ -6,5 +6,5 @@ db_name: str = 'DisasterResponse.db'
 table_name: str = db_name[:-3]
 db_path: Path = project_root / 'src' / 'data' / db_name
 
-model_name: str = 'classifier_tfidf_rf.pkl'
+model_name: str = 'classifier.pkl'
 model_path: Path = project_root / 'src' / 'models' / model_name
